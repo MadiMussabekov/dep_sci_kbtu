b21a19a08ef2f33da28df753fd3000d94c7c73ee
Madi <walparis1@mail.com>
2024-04-02 14:13:47 +0500

    tailwind config

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -18,12 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          inter.variable
-        )}
-      >
+      <body className={cn("min-h-screen bg-background font-sans antialiased")}>
         {children}
       </body>
     </html>
