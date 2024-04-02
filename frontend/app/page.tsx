42b33f0c8f8d03a2ea6155a1480faee9bf7b057e
Madi <walparis1@mail.com>
2024-04-02 14:14:31 +0500

    faculty card

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,31 @@
 import Image from "next/image";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
+import FacultyCard from "./components/faculty-card";
+
 export default function Home() {
+  let facultyInfo = [
+    {
+      imageUrl: "/img/card-photo.png",
+      facultyName: "ИТ",
+      facultyDesc: "РАЗРАБОТКА ПО, ИИ, РАБОТОТЕХНИКА, КВАНТОВЫЕ ВЫЧИСЛЕНИЯ",
+    },
+    {
+      imageUrl: "/img/card-photo.png",
+      facultyName: "ЭНЕРГЕТИКА",
+      facultyDesc: "ВОЗОБНОВЛЯЕМЫЕ ИСТОЧНИКИ ЭНЕРГИИ, ЯДЕРНАЯ ЭНЕРГИЯ",
+    },
+  ];
   return (
     <div>
       <p>Hello</p>
-      <Navbar></Navbar>
-      <Footer></Footer>
+      {/* <Navbar></Navbar>
+      <Footer></Footer> */}
+      <div className="flex items-center">
+        {facultyInfo.map((faculty, index) => (
+          <FacultyCard key={index} {...faculty}></FacultyCard>
+        ))}
+      </div>
     </div>
   );
 }
