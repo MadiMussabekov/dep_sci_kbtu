752e6c6eafa25750e572a64b994fea64a228e61f
Madi <walparis1@mail.com>
2024-07-18 00:02:32 +0500

    123

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,17 +1,17 @@
-// The middleware matches a locale for the request and handles redirects 
+// The middleware matches a locale for the request and handles redirects
 // and rewrites accordingly.
 
-import createMiddleware from 'next-intl/middleware';
- 
+import createMiddleware from "next-intl/middleware";
+
 export default createMiddleware({
   // A list of all locales that are supported
-  locales: ['en', 'kk', 'ru'],
- 
+  locales: ["en", "kk", "ru"],
+
   // Used when no locale matches
-  defaultLocale: 'en'
+  defaultLocale: "en",
 });
- 
+
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/', '/(kk|ru|en)/:path*']
+  matcher: ["/", "/(kk|ru|en)/:path*"],
 };
