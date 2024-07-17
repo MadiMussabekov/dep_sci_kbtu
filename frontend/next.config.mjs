be2fb173419fd5d885d963d8eb9ea2bbcafa0f9c
Madi <walparis1@mail.com>
2024-07-17 22:02:14 +0500

    render

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,14 +3,17 @@
 
 // export default nextConfig;
 
-// Now, set up the plugin which creates an alias to provide your i18n 
+// Now, set up the plugin which creates an alias to provide your i18n
 // configuration (specified in the next step) to Server Components.
 
-import createNextIntlPlugin from 'next-intl/plugin';
- 
+import createNextIntlPlugin from "next-intl/plugin";
+
 const withNextIntl = createNextIntlPlugin();
- 
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
- 
+const nextConfig = {
+  output: "export",
+  reactStrictMode: true,
+};
+
 export default withNextIntl(nextConfig);
